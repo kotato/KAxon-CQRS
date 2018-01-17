5e8dce162abcd4cd1c84c88c2b7ad8b4f5ab3e95
Ramon Aranda <ramon.aranda.r@gmail.com>
2018-01-17 18:02:45 +0100

    Add todo at query bus

--- src/main/kotlin/com/kotato/cqrs/domain/query/QueryBus.kt
+++ src/main/kotlin/com/kotato/cqrs/domain/query/QueryBus.kt
@@ -6,6 +6,7 @@ interface QueryBus {
     fun <T> ask(query: Query, klass: KClass<*>): T
 }
 
+//TODO: refactor this when traits are available for kotlin
 inline fun <reified T> QueryBus.ask(query: Query): T {
     return this.ask(query, T::class)
 }
